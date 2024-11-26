7498ce8a894476009ad1a231017610654ea92314
B.K. Masinde <43029260+BMasinde@users.noreply.github.com>
2024-11-26 14:52:53 +0100

    Update synth_data_gen.R

--- synth_data_gen.R
+++ synth_data_gen.R
@@ -1,6 +1,9 @@
 
 # Synthetic Data Gen
 
+# setting seed
+set.seed(1234)
+
 # Number of observations
 n <- 15 
 
@@ -32,9 +35,37 @@ cattle <- sample(0:40, size = n, replace = TRUE)
 # chronic illness?
 illness <- sample(c("Yes", "No"), size = n, replace = TRUE, prob = c(0.15, 0.85))
 
-synthetic_data <- cbind(first_name, children, age, cattle, illness)
+# house structure:
+house_type <- c("permanent", "traditional", "traditional", "permanent",
+                "permanent", "traditional", "permanent", "traditional",
+                "traditional", "traditional", "permanent", "traditional",
+                "traditional", "traditional", "traditional")
 
-synthetic_data <- cbind(synthetic_data, phone_numbers)
+
+# needs re-assement date
+date <- sample(seq.Date(as.Date("2024-12-01"), as.Date("2024-12-31"), by = "day"), 15)
+
+# primary hazards
+hazard <- c(rep("flooding", n))
+
+# secondary hazard effects
+secondary_effects <- c("Malaria", "Cholera", "Livestock diseases", "Malaria",
+                       "Malaria", "Cholera", "Cholera", "Malaria", "Cholera",
+                       "Livestock diseases", "Cholera", "Malaria", "Malaria",
+                       "Malaria", "Cholera")
+
+# Amounts received
+Jan <- c(trunc(runif(15, min = 400, max = 1200)))
+Feb <- c(trunc(runif(15, min = 400, max = 1200)))
+March <- c(trunc(runif(15, min = 400, max = 1200)))
+April <- c(trunc(runif(15, min = 400, max = 1200)))
+May <- c(trunc(runif(15, min = 400, max = 1200)))
+June <-  c(trunc(runif(15, min = 400, max = 1200)))
+July <- c(trunc(runif(15, min = 400, max = 1200)))
+August <- c(trunc(runif(15, min = 400, max = 1200)))
+Sept <- c(trunc(runif(15, min = 400, max = 1200)))
+Oct <-  c(trunc(runif(15, min = 400, max = 1200)))
+Nov <-  c(trunc(runif(15, min = 400, max = 1200)))
 
 image_names <- c("Brian.jpeg", "Mary.jpeg", "Mathew.jpeg", "Maria.jpeg", 
                  "Susan.jpeg", "Trudy.jpeg", "Jayden.jpeg",
@@ -42,6 +73,20 @@ image_names <- c("Brian.jpeg", "Mary.jpeg", "Mathew.jpeg", "Maria.jpeg",
                  "Abigael.jpeg", "Jessica.jpeg", "Steven.jpeg", 
                  "Michael.jpeg")
 
-synthetic_data <- cbind(synthetic_data,image_names)
+#synthetic_data <- cbind(first_name, children, age, cattle, illness)
+
+#synthetic_data <- cbind(synthetic_data, phone_numbers)
+
+
+
+#synthetic_data <- cbind(synthetic_data,image_names)
+
+synthetic_data <- data.frame(first_name, children, age, cattle, illness,
+                        phone_numbers, image_names, house_type,
+                        date, hazard, secondary_effects, Jan, Feb,
+                        March, April, May, June, July, August, Sept,
+                        Oct, Nov)
+
+
 
 write.csv(synthetic_data, file = "/Users/masinde/Projects/cash_based_assistance/data/synthetic_data.csv")
